0024d1eb8374aa9076ff8431489239721251aae3
BroopDias <ibroopi@gmail.com>
2016-06-30 09:48:32 +0100

    Added controller

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -1,7 +1,51 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: broop
- * Date: 23/06/16
- * Time: 14:34
- */
+
+namespace OuterEdge\Layout\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use OuterEdge\Layout\Model\Elements;
+
+class Index extends Action
+{
+    /**
+     * @var OuterEdge\Layout\Model\ElementsFactory
+     */
+    protected $_modelElementsFactory;
+
+    /**
+     * @param Context $context
+     * @param ElementsFactory $_modelElementsFactory
+     */
+    public function __construct(
+        Context $context,
+        ElementsFactory $modelElementsFactory
+    ) {
+        parent::__construct($context);
+        $this->_modelElementsFactory = $modelElementsFactory;
+    }
+
+    public function execute()
+    {
+        /**
+         * When Magento get your model, it will generate a Factory class
+         * for your model at var/generaton folder and we can get your
+         * model by this way
+         */
+        $elementsModel = $this->_modelElementsFactory->create();
+
+        // Load the item with ID is 1
+        $element = $elementsModel->load(1);
+        if($element){
+            echo $element->getName() . " found with Id 1";
+            echo "<hr/>";
+        }
+
+        // Get element collection
+        $elementsCollection = $elementsModel->getCollection();
+        // Load all data of collection
+        foreach($elementsCollection as $element){
+            echo $element->getName() . " , " . $element->getTitle() . "<br/>";
+        }
+    }
+}
