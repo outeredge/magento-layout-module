bb2c51957393072bd7ff0288a27e97c495625f54
minlare <mike@outeredgeuk.com>
2017-08-22 15:04:00 +0100

    helper class functions

--- Helper/Data.php
+++ Helper/Data.php
@@ -3,67 +3,105 @@
 namespace OuterEdge\Layout\Helper;
 
 use Magento\Framework\App\Helper\AbstractHelper;
-use OuterEdge\Layout\Model\ElementFactory;
+use Magento\Framework\App\Helper\Context;
 use OuterEdge\Layout\Model\GroupFactory;
+use OuterEdge\Layout\Model\Group;
+use OuterEdge\Layout\Model\ResourceModel\Group\CollectionFactory as GroupCollectionFactory;
+use OuterEdge\Layout\Model\ElementFactory;
+use OuterEdge\Layout\Model\Element;
+use OuterEdge\Layout\Model\ResourceModel\Element\CollectionFactory as ElementCollectionFactory;
 
 class Data extends AbstractHelper
 {
     /**
-     * @var ElementFactory
-     */
-    protected $_modelElementFactory;
-
-    /**
      * @var GroupFactory
      */
-    protected $_modelGroupFactory;
+    protected $groupFactory;
 
     /**
-     *
-     * @param ElementFactory $modelElementFactory
-     * @param GroupFactory $modelGroupFactory
+     * @var GroupCollectionFactory
+     */
+    protected $groupCollectionFactory;
+
+    /**
+     * @var ElementFactory
+     */
+    protected $elementFactory;
+
+    /**
+     * @var ElementCollectionFactory
+     */
+    protected $elementCollectionFactory;
+
+    /**
+     * @param Context $context
+     * @param GroupFactory $groupFactory
+     * @param GroupCollectionFactory $groupCollectionFactory
+     * @param ElementFactory $elementFactory
+     * @param ElementCollectionFactory $elementCollectionFactory
      */
     public function __construct(
-        ElementFactory $modelElementFactory,
-        GroupFactory $modelGroupFactory
+        Context $context,
+        GroupFactory $groupFactory,
+        GroupCollectionFactory $groupCollectionFactory,
+        ElementFactory $elementFactory,
+        ElementCollectionFactory $elementCollectionFactory
     ) {
-        $this->_modelElementFactory = $modelElementFactory;
-        $this->_modelGroupFactory   = $modelGroupFactory;
+        $this->groupFactory = $groupFactory;
+        $this->groupCollectionFactory = $groupCollectionFactory;
+        $this->elementFactory = $elementFactory;
+        $this->elementCollectionFactory = $elementCollectionFactory;
+        parent::__construct($context);
     }
 
     /**
-     * @param string $groupCode
-     * @return array
+     * @return Group
      */
-    public function getLayoutContents($groupCode = false)
+    public function getGroup($id = false, $field = null)
     {
-        $groupModel = $this->_modelGroupFactory->create();
-
-        $groupData = $groupModel->getGroupIdByCode($groupCode);
-        if (!$groupData) {
-            return null;
+        $group = $this->groupFactory->create();
+        if ($id) {
+            $group->load($id, $field);
         }
-
-        $elementModel = $this->_modelElementFactory->create();
-        $result = $elementModel->loadByGroup($groupData->getGroupId());
-
-        $data = $groupData->getData();
-        $data['element'] = $this->groupBy($result->getData(), 'title');
-
-        return $data;
+        return $group;
     }
 
     /**
-     * @param array $array
-     * @param string $key
-     * @return array
+     * @return Element
      */
-    protected function groupBy($array, $key)
+    public function getElement($id = false, $field = null)
     {
-        $return = [];
-        foreach ($array as $val) {
-            $return[$val[$key]] = $val;
+        $element = $this->elementFactory->create();
+        if ($id) {
+            $element->load($id, $field);
         }
-        return $return;
+        return $element;
+    }
+
+    /**
+     * @return GroupCollection
+     */
+    public function getGroupCollection()
+    {
+        return $this->groupCollectionFactory->create();
+    }
+
+    /**
+     * @return ElementCollection
+     */
+    public function getElementCollection()
+    {
+        return $this->elementCollectionFactory->create();
+    }
+
+    /**
+     * @return Group
+     */
+    public function getGroupAndElements($id, $field = null)
+    {
+        $group = $this->getGroup($id, $field);
+        $elements = $this->getElementCollection()->addFieldToFilter('group_id', ['eq' => $group->getId()]);
+        $group->setData('elements', $elements);
+        return $group;
     }
 }
